c2791431d1cb404e96da84bc670c3be5e72ab98f
luke2327 <qwepoi8526@gmail.com>
2018-06-15 20:32:17 +0900

    modified:   templates/result.html 	modified:   untitled.py

--- untitled.py
+++ untitled.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, request
-
+from flask import Flask, render_template, request, url_for, request
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,8 +9,11 @@ def home():
 @app.route('/per_side',methods = ['POST', 'GET'])
 def per_side():
 	result = request.form
-	content = request.values["content"]
-	print content
+	print request.content
+	d = datetime.datetime.now()
+	k = str(d.year)+str(d.day)+str(d.hour)+str(d.minute)+str(d.second)
+	f = open(k+'.html', 'w')
+	f.write(render_template("result.html", result = result).encode('utf-8'))
 	return render_template("result.html", result = result)
 
 
