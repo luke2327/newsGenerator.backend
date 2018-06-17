cca986bcf7a2ee785a65df2f6d0fbaa275913591
System Administrator <root@Luke2327ui-MacBook-Pro.local>
2018-06-18 00:28:52 +0900

    new file:   html/201817225827.html 	new file:   html/20181801326.html 	new file:   html/20181801414.html 	new file:   html/20181801618.html 	new file:   html/20181801721.html 	new file:   html/20181801816.html 	new file:   html/20181801844.html 	new file:   html/20181802819.html 	modified:   templates/index.html 	modified:   templates/result.html 	modified:   untitled.py

--- untitled.py
+++ untitled.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request, url_for, request
 import datetime
+import os
+import boto3
+import logging
+
+s3 = boto3.resource('s3')
+bucket_name = 'rbtest2'
 
 app = Flask(__name__)
 
@@ -12,8 +18,25 @@ def result():
 	result = request.form
 	d = datetime.datetime.now()
 	k = str(d.year)+str(d.day)+str(d.hour)+str(d.minute)+str(d.second)
-	f = open(k+'.html', 'w')
-	f.write(render_template("result.html", result = result).encode('utf-8'))
+        key = k+'.html'
+	os.chdir('./html/')
+        try:
+	    f = open(key, 'w')
+	    f.write(render_template("result.html", result = result).encode('utf-8'))
+            p
+        except Exception as e:
+            logging.error(e)
+            print e
+        finally:
+            f.close()
+
+        try:
+            f = open(key, 'rb')
+            s3.Bucket(bucket_name).put_object(Key=key, Body=f)
+        except Exception as e:
+            print e
+        finally:
+            f.close()
 	return render_template("result.html", result = result)
 
 
