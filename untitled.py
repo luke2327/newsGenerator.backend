f483213503cd3e85540ee669f0c839cf92ecb53c
luke2327 <qwepoi8526@gmail.com>
2018-06-18 11:51:59 +0900

    modified:   untitled.py

--- untitled.py
+++ untitled.py
@@ -15,51 +15,55 @@ def home():
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-	result = request.form
-        language = result.get('language_cd')
-	d = datetime.datetime.now()
-	news_name = str(d.year)+str(d.day)+str(d.hour)+str(d.minute)+str(d.second)
-        object_key = news_name+'.html'
-        path_check = str((os.path.dirname(os.path.realpath(__file__)))).split('/')[-1]
-        if path_check != 'html':
-	    os.chdir('html/')
+  result = request.form
+  language = result.get('language_cd')
+  d = datetime.datetime.now()
+  news_name = str(d.year)+str(d.day)+str(d.hour)+str(d.minute)+str(d.second)
+  object_key = news_name+'.html'
+  path_check = str((os.path.dirname(os.path.realpath(__file__)))).split('/')[-1]
+  if path_check != 'html':
+    os.chdir('html/')
 
-        try:
-	    f = open(object_key, 'w')
-	    f.write(render_template("result.html", result = result).encode('utf-8'))
-        except Exception as e:
-            logging.error(e)
-            print e
-        finally:
-            f.close()
+  try:
+    f = open(object_key, 'w')
+    f.write(render_template("result.html", result = result).encode('utf-8'))
 
-        try:
-            data = open(object_key, 'rb')
-            if language == 'EN':
-                s3.Bucket(bucket_name).put_object(Key='news/en/'+object_key, Body=data)
-                s3.ObjectAcl(bucket_name, 'news/en/'+object_key).put(ACL='public-read')
-            elif language == 'ID':
-                s3.Bucket(bucket_name).put_object(Key='news/id/'+object_key, Body=data)
-                s3.ObjectAcl(bucket_name, 'news/id/'+object_key).put(ACL='public-read')
-            elif language == 'VI':
-                s3.Bucket(bucket_name).put_object(Key='news/vi/'+object_key, Body=data)
-                s3.ObjectAcl(bucket_name, 'news/vi/'+object_key).put(ACL='public-read')
-            elif language == 'TH':
-                s3.Bucket(bucket_name).put_object(Key='news/th/'+object_key, Body=data)
-                s3.ObjectAcl(bucket_name, 'news/th/'+object_key).put(ACL='public-read')
-            elif language == 'BR':
-                s3.Bucket(bucket_name).put_object(Key='news/br/'+object_key, Body=data)
-                s3.ObjectAcl(bucket_name, 'news/br/'+object_key).put(ACL='public-read')
-            elif language == 'KO':
-                s3.Bucket(bucket_name).put_object(Key='news/ko/'+object_key, Body=data)
-                s3.ObjectAcl(bucket_name, 'news/ko/'+object_key).put(ACL='public-read')
+  except Exception as e:
+    logging.error(e)
+    print e
 
-        except Exception as e:
-            logging.error(e)
-            print e
-        finally:
-            data.close()
-	return render_template("result.html", result = result)
+  finally:
+    f.close()
+
+  try:
+    data = open(object_key, 'rb')
+    if language == 'EN':
+      s3.Bucket(bucket_name).put_object(Key='news/en/'+object_key, Body=data)
+      s3.ObjectAcl(bucket_name, 'news/en/'+object_key).put(ACL='public-read')
+    elif language == 'ID':
+      s3.Bucket(bucket_name).put_object(Key='news/id/'+object_key, Body=data)
+      s3.ObjectAcl(bucket_name, 'news/id/'+object_key).put(ACL='public-read')
+    elif language == 'VI':
+      s3.Bucket(bucket_name).put_object(Key='news/vi/'+object_key, Body=data)
+      s3.ObjectAcl(bucket_name, 'news/vi/'+object_key).put(ACL='public-read')
+    elif language == 'TH':
+      s3.Bucket(bucket_name).put_object(Key='news/th/'+object_key, Body=data)
+      s3.ObjectAcl(bucket_name, 'news/th/'+object_key).put(ACL='public-read')
+    elif language == 'BR':
+      s3.Bucket(bucket_name).put_object(Key='news/br/'+object_key, Body=data)
+      s3.ObjectAcl(bucket_name, 'news/br/'+object_key).put(ACL='public-read')
+    elif language == 'KO':
+      s3.Bucket(bucket_name).put_object(Key='news/ko/'+object_key, Body=data)
+      s3.ObjectAcl(bucket_name, 'news/ko/'+object_key).put(ACL='public-read')
+
+  except Exception as e:
+    logging.error(e)
+    print e
+
+  finally:
+    data.close()
+
+  return render_template("result.html", result = result)
 
 
 if __name__ == '__main__':
