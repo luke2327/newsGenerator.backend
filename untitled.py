5b259d6a0d5706cd42c88a37665ef59c6ae1b5df
luke2327 <https://github.com/luke2327>
2018-06-19 08:38:55 +0000

    modified:   templates/result.html 	modified:   untitled.py

--- untitled.py
+++ untitled.py
@@ -5,6 +5,7 @@ import os
 import boto3
 import logging
 
+
 s3 = boto3.resource('s3')
 bucket_name = 'rbtest2'
 app = Flask(__name__)
@@ -14,62 +15,61 @@ def current_time():
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result' ,methods = ['POST', 'GET'])
 def result():
-  result = request.form
-  print result[0]
-  d = datetime.datetime.now()
-  news_name = str(d.year) + str(d.day) + str(d.hour) + str(d.minute) + str(d.second)
-  object_key = 'news/' + result.get('language_cd').lower() + '/' + news_name + '.html'
-  saving_key = news_name + '.html'
-  print current_time() + 'You have successfully entered.'
-  print current_time() + '[Defined data] language : ' + result.get('language_cd') + ', object_key : ' + saving_key
-  if str((os.path.dirname(os.path.realpath(__file__)))).split('/')[-1].split('\\')[-1] != 'html':
-    os.chdir('html/')
+    result = request.form
+    d = datetime.datetime.now()
+    news_name = str(d.year) + str(d.day) + str(d.hour) + str(d.minute) + str(d.second)
+    object_key = 'news/' + result.get('language_cd').lower() + '/' + news_name + '.html'
+    saving_key = news_name + '.html'
+    print current_time() + 'You have successfully entered.'
+    print current_time() + '[Defined data] language : ' + result.get('language_cd') + ', object_key : ' + saving_key
+    if str((os.path.dirname(os.path.realpath(__file__)))).split('/')[-1].split('\\')[-1] != 'html':
+        os.chdir('html/')
 
-  try:
-    print current_time() + 'The file save_data will be opened.'
-    save_data = open(saving_key, 'w')
-    save_data.write(render_template("result.html", result = result).encode('utf-8'))
+    try:
+        print current_time() + 'The file save_data will be opened.'
+        save_data = open(saving_key, 'w')
+        save_data.write(render_template("result.html", result = result).encode('utf-8'))
 
-  except Exception as e:
-    logging.error(e)
-    print current_time() + 'Failed to save result_file.'
-    print e
+    except Exception as e:
+        logging.error(e)
+        print current_time() + 'Failed to save result_file.'
+        print e
 
-  else:
-    print current_time() + 'The result_file was successfully saved to html/'
+    else:
+        print current_time() + 'The result_file was successfully saved to html/'
 
-  finally:
-    print current_time() + 'The file save_data will be closed.'
-    save_data.close()
+    finally:
+        print current_time() + 'The file save_data will be closed.'
+        save_data.close()
 
-  try:
-    print current_time() + 'The file s3_data will be opened.'
-    data = open(saving_key, 'rb')
-    s3.Bucket(bucket_name).put_object(Key=object_key, Body=data)
-    s3_object = s3.Object(bucket_name, object_key)
-    s3_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': object_key},
-                        MetadataDirective="REPLACE",
-                        ContentType="text/html")
-    s3.ObjectAcl(bucket_name, object_key).put(ACL='public-read')
+    try:
+        print current_time() + 'The file s3_data will be opened.'
+        data = open(saving_key, 'rb')
+        s3.Bucket(bucket_name).put_object(Key=object_key, Body=data)
+        s3_object = s3.Object(bucket_name, object_key)
+        s3_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': object_key},
+                            MetadataDirective="REPLACE",
+                            ContentType="text/html")
+        s3.ObjectAcl(bucket_name, object_key).put(ACL='public-read')
 
-  except Exception as e:
-    logging.error(e)
-    print current_time() + 'Upload failed on S3'
-    print e
+    except Exception as e:
+        logging.error(e)
+        print current_time() + 'Upload failed on S3'
+        print e
 
-  else:
-    print current_time() + saving_key + ' was successfully uploaded to S3.'
+    else:
+        print current_time() + saving_key + ' was successfully uploaded to S3.'
 
-  finally:
-    print current_time() + 'The file s3_data will be closed.'
-    data.close()
+    finally:
+        print current_time() + 'The file s3_data will be closed.'
+        data.close()
 
-  return render_template("result.html", result = result)
+    return render_template("result.html", result = result)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
