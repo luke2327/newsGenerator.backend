c678a2584b79447792d8f8a4f8e4dc32eb957f97
luke2327 <https://github.com/luke2327>
2018-06-21 08:33:43 +0000

    s3 storage changed

--- untitled.py
+++ untitled.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, url_for, request
+from flask import Flask, render_template, request, url_for
 from werkzeug.utils import secure_filename
-import datetime
+from datetime import datetime, timedelta
 import os
+import pytz
 import boto3
 import logging
 logging.basicConfig(filename='untitled.log', level=logging.INFO,
                    format='%(asctime)s - %(levelname)s - %(message)s',
                    datefmt='%Y-%m-%d %I:%M:%S %p')
 
-s3 = boto3.resource('s3')
-bucket_name = 'rbtest2'
-app = Flask(__name__)
-
 UPLOAD_FOLDER = './download'
 ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png', 'gif'])
+
+s3 = boto3.resource('s3')
+bucket_name = 'content.swips.co'
+
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def current_time():
-    return '[' + str(datetime.datetime.now()).split('.')[0] + '] '
+    return '[' + (datetime.now(pytz.utc) +
+                  timedelta(hours=9)).strftime('%Y-%m-%d %H:%M:%S') + '] '
 
 def allowed_file(filename):
-    return '.' in filename.lower() and filename.lower().rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename.lower() and\
+                  filename.lower().rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
@@ -32,28 +37,36 @@ def result():
     result = request.form
     file_s = request.files['image_link']
     filename = ''
-    d = datetime.datetime.now()
-    news_name = str(d.year) + str(d.day) + str(d.hour) + str(d.minute) + str(d.second)
+    news_name = (datetime.now(pytz.utc) + timedelta(hours=9)).strftime('%d%H%M%S')
+    dirc_name = (datetime.now(pytz.utc) + timedelta(hours=9)).strftime('%Y%m')
+
     for key, value in result.iteritems():
         print '[' + str(key.encode('utf-8')) + '] ' + str(value.encode('utf-8'))
         logging.info('[' + str(key.encode('utf-8'))+ '] ' + str(value.encode('utf-8')))
+
+
     if file_s and allowed_file(file_s.filename):
         file_s.filename = news_name + '.' +\
         file_s.filename.split('.')[1].lower()
         filename = secure_filename(file_s.filename)
         file_s.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    img_object_key = 'news/' + result.get('language_cd').lower() + '/image/' + filename
+
+
+    img_object_key = 'images/' + dirc_name + '/' + filename
     img_saving_key = 'download/' + filename
-    doc_object_key = 'news/' + result.get('language_cd').lower() + '/' + news_name + '.html'
+    doc_object_key = 'news/' + dirc_name + '/' + news_name + '.html'
     doc_saving_key = 'html/' + news_name + '.html'
     print current_time() + 'You have successfully entered.'
     print current_time() + '[Defined data] language : ' +\
-                            result.get('language_cd') + ', object_key : ' + doc_saving_key
+                            result.get('language_cd') +\
+                            ', object_key : ' + doc_saving_key
 
     try:
         print current_time() + 'The file save_data will be opened.'
+        file_s.filename = dirc_name + '/' + filename
         save_data = open(doc_saving_key, 'w')
-        save_data.write(render_template("result.html", result = result, file_s = file_s).encode('utf-8'))
+        save_data.write(render_template("result.html",
+                                        result = result, file_s = file_s).encode('utf-8'))
 
     except Exception as e:
         logging.error(e)
@@ -93,8 +106,9 @@ def result():
         print current_time() + doc_saving_key + ' was successfully uploaded to S3.'
 
     finally:
-        print current_time() + 'The file s3_data will be closed.'
+        print current_time() + 'The file data will be closed.'
         data_doc.close()
+        data_img.close()
 
     return render_template("result.html", result = result, file_s = file_s)
 
